e551e6e415ac131cad9fbf22b68381fa82690932
Marko Jurincic <marko.jurincic@gmail.com>
2014-09-01 16:56:48 +0200

    Changed date format

--- app/controllers/m3_table_admin/invoices_controller.rb
+++ app/controllers/m3_table_admin/invoices_controller.rb
@@ -9,7 +9,7 @@ class M3TableAdmin::InvoicesController < M3TableAdmin::ApplicationController
       @table.add_autocomplete_column "our_company", "name"
       @table.add_column "invoice_number", "text"
       @table.add_column "price", "text"
-      @table.add_timestamp_column "sent_at", "%Y-%m-%d %H:%M:%S"
+      @table.add_timestamp_column "sent_at", "%Y-%m-%d"
 
     end
 end
