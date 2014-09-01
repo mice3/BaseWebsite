0b26848043d85d14a31747870a227f8fa10d56c3
Marko Jurincic <marko.jurincic@gmail.com>
2014-09-01 16:06:04 +0200

    Table admin payments sent at

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140901102432) do
+ActiveRecord::Schema.define(version: 20140901104033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140901102432) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.datetime "spended_at"
   end
 
   create_table "images", force: true do |t|
@@ -61,6 +62,7 @@ ActiveRecord::Schema.define(version: 20140901102432) do
     t.datetime "updated_at"
     t.integer  "our_company_id"
     t.string   "invoice_number"
+    t.datetime "sent_at"
   end
 
   create_table "oauths", force: true do |t|
@@ -122,6 +124,7 @@ ActiveRecord::Schema.define(version: 20140901102432) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "payed_at"
   end
 
   create_table "projects", force: true do |t|
