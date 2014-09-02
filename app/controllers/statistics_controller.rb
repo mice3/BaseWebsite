204294df20b4394234a1ec7846c4724653adba8a
Marko Jurincic <marko.jurincic@gmail.com>
2014-09-02 08:40:34 +0200

    Earnings beta

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -6,15 +6,26 @@ class StatisticsController < ApplicationController
     date_key = ""
     @monthly_earnings = {}
 
+
     @earnings.each do |earning|
       earning_date = earning.payed_at.strftime("%m-%Y")
       if @monthly_earnings.has_key?(earning_date)
         @monthly_earnings[earning_date] = @monthly_earnings[earning_date] + earning.price
       else
         @monthly_earnings[earning_date] = earning.price
-
       end
     end
+
+    starting_date = Date.parse("1-2-2012")
+
+
+    while starting_date.strftime("%m-%Y") != Date.today.strftime("%m-%Y") do
+      if !@monthly_earnings.has_key?(starting_date.strftime("%m-%Y"))
+        @monthly_earnings[starting_date.strftime("%m-%Y")] = 0
+      end
+      starting_date = starting_date + 1.month
+    end
+
   end
 
 end
