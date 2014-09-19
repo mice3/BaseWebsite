e43f38da2d0309020dc9b102efa176dc8489366d
Marko Jurincic <marko.jurincic@gmail.com>
2014-09-19 20:48:16 +0200

    Added net salary

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  def money_sum_gross(current, object)
+    if object
+      object = object.gross
+    else
+      object = 0
+    end
+    return current + (object * 3)
+  end
+
+  def money_sum_net(current, object)
+    if object
+      object = object.net
+    else
+      object = 0
+    end
+    return current + (object * 3)
+  end
+
 end
