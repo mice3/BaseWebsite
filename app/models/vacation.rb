d2b46d6ef7b881251ef975e4f758bebcdc32b1e0
Marko Jurincic <marko.jurincic@gmail.com>
2014-09-19 15:47:13 +0200

    Add vacation adding on scrum

--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -1,3 +1,8 @@
 class Vacation < ActiveRecord::Base
   belongs_to :user
+
+  scope :for_user_at_date,-> user_id, date {
+    joins(:user)
+    .where("email = ? and ? BETWEEN start_date AND end_date",user_id, date)
+  }
 end
